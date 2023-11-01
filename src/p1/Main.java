08b5ab689e0cb1a6a434835ec9e221db94713978
asrytikov <asrytikov@gmail.com>
2023-11-01 10:48:20 +0400

    Initial commit

---
+++ src/p1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Thread thread = Thread.currentThread();
+        System.out.println(thread);
+        System.out.println(thread.getName());
+        System.out.println(thread.getPriority());
+        System.out.println(thread.getThreadGroup().getName());
+    }
+}
